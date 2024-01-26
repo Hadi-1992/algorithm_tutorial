6dd2d3d30d8206a818e14fb69b4dc187bfe778fc
Hadi <mansoorhadi32@gmail.com>
2024-01-26 11:30:57 +0330

    create selection sort

---
+++ src/main/java/selectionsort/SelectionSort.java
@@ -0,0 +1,28 @@
+package selectionsort;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 85, 22, 11, 1, 70, 99, 14};
+        selectionSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
